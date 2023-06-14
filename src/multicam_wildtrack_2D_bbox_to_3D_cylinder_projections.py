9da5d5a2196e2bd0d612fe038b12ec3f5648abb2
tostenzel <tobias.stenzel@mailbox.org>
2023-06-14 14:38:18 +0200

    Almost complete and prettify projection modules

--- src/multicam_wildtrack_2D_bbox_to_3D_cylinder_projections.py
+++ src/multicam_wildtrack_2D_bbox_to_3D_cylinder_projections.py
@@ -1,6 +1,10 @@
+"""Transform 2D image plane bbox parameters to 3D world grid cylinder params.
 
+Notation follows Duy's thesis, Appendix B in
+https://arxiv.org/pdf/2111.11892.pdf.
 
-from typing import Tuple
+"""
+from typing import Dict, Tuple
 
 import numpy as np
 from cv2 import Rodrigues
@@ -8,20 +12,31 @@ from numpy.linalg import inv#, pinv
 from scipy.optimize import minimize
 
 
+def transform_2D_bbox_to_3D_cylinder_params(
+    bbox: Dict,
+    rvec: np.array,
+    tvec: np.array,
+    camera_matrix: np.array
+) -> Dict:
+    """Transforms 2D bbox to 3D cylinder given camera calibration.
 
+    Args:
+        bbox: bbox data with xmax, xmin, ymax, ymin,
+            origin in upper left corner.
+        rvec: camera rotation vector (extrinsic parameters).
+        tvec: camera translation vector (extrinsic paramters).
+        camera_matrix: camera matrix (intrinsic paramters).
 
+    Returns:
+        cylinder params.
 
-
-
-
-
-def transform_2D_bbox_to_3D_cylinder_params(bbox, rvec, tvec, camera_matrix):
-    x_foot = (bbox["xmax"] + bbox["xmin"]) / 2 
+    """
+    x_foot = (bbox["xmax"] + bbox["xmin"]) / 2
     y_foot = bbox["ymax"]
-    X_foot, _ = project_2D_to_3D(x_foot, y_foot, rvec, tvec, camera_matrix)
+    X_foot, _ = _project_2D_to_3D(x_foot, y_foot, rvec, tvec, camera_matrix)
     X_center = X_foot[0]
     Y_center = X_foot[1]
-    Height = get_cylinderheight_from_bbox(
+    Height = _get_cylinderheight_from_bbox(
         x_foot,
         bbox["ymin"],
         bbox["ymax"],
@@ -29,7 +44,12 @@ def transform_2D_bbox_to_3D_cylinder_params(bbox, rvec, tvec, camera_matrix):
         tvec,
         camera_matrix
     )
-    X_lowerright_corner, _  = project_2D_to_3D(bbox["xmin"], bbox["ymax"], rvec, tvec, camera_matrix)
+    X_lowerright_corner, _  = _project_2D_to_3D(
+        bbox["xmin"],
+        bbox["ymax"],
+        rvec, tvec,
+        camera_matrix
+    )
     Radius = np.linalg.norm(X_lowerright_corner[0:2] - X_foot[0:2])
     return {
         "x_center": X_center,
@@ -39,7 +59,7 @@ def transform_2D_bbox_to_3D_cylinder_params(bbox, rvec, tvec, camera_matrix):
     }
 
 
-def project_2D_to_3D(
+def _project_2D_to_3D(
         x: float,
         y: float,
         rvec: np.array,
@@ -48,11 +68,8 @@ def project_2D_to_3D(
 ) -> Tuple[np.array, np.array]:
     """Project 2D image plane point to 3D world grid.
 
-    Notation follows Duy's thesis, Appendix B in
-    https://arxiv.org/pdf/2111.11892.pdf.
-
     Commented out can test an equivalence relation.
-    
+
     Args:
         x: image x
         y: image y, origin is top left corner
@@ -63,41 +80,52 @@ def project_2D_to_3D(
     Returns:
         3D projection of image
         camera center in 3D worldcoordinates
+
     """
-    
     #def get_Rt(R, tvec):
     #    return np.concatenate((R, tvec), axis=1)
 
-    x_homogenous = np.float32([x, y, 1]).reshape((3,1))
+    x_homogenous = np.float32([x, y, 1]).reshape((3,1)) # pylint: disable=[E1121]
     R, _ = Rodrigues(rvec)
     #Rt = get_Rt(R, tvec)
     #P = K @ Rt
     M = K @ R
 
-    # Ctilde coordinates of the camera centre C in the world coordinate 
+    # Ctilde coordinates of the camera centre C in the world coordinate
     Ctilde = - R.T @ tvec
-    #np.testing.assert_almost_equal(Ctilde,- inv(M) @ P[:,3].reshape((3,1)), decimal=4)
+    #np.testing.assert_almost_equal(
+    #    Ctilde,
+    #    - inv(M) @ P[:,3].reshape((3,1)), decimal=4
+    #)
     #Pplus0 = pinv(P)
     Xtilde = inv(M) @ x_homogenous
     Xtilde_pi = - (Ctilde[2] / Xtilde[2]) * (inv(M) @ x_homogenous) + Ctilde
-  
+
     return Xtilde_pi.flatten(), Ctilde.flatten()
 
 
-def decode_3D_cylinder_center(id: int) -> np.array:
+def _decode_3D_cylinder_center(id_: int) -> np.array:
     """Decode WILDTRACK positionID into 3d world grid foot point.
-    
-    Taken from Wildtrack Readme but with intercept and coefficient times scale := 100.
+
+    Taken from Wildtrack Readme but with intercept and coefficient times
+    scale := 100.
     2.5cm is one grid point step, 480 is image height.
+
+    Args:
+        id_: WILDTRACK id that encodes x and y in (x,y,0) in 3D world grid.
+
+    Returns:
+        3D world coordinates of bbox object with respective id.
+
     """
-    x_grid = -300 + 2.5 * (id % 480)
-    y_grid = -900 + 2.5 * (id / 480)
+    x_grid = -300 + 2.5 * (id_ % 480)
+    y_grid = -900 + 2.5 * (id_ / 480)
     return np.float32([[x_grid, y_grid, 0]])
 
 
-def get_cylinderheight_from_bbox(x_avg, ymin, ymax, rvec, tvec, K):
-    """lololol
-    
+def _get_cylinderheight_from_bbox(x_avg, ymin, ymax, rvec, tvec, K):
+    """Compute heigth of 3D cylinder from bbox parameters.
+
     Args:
         x_avg: bbox midpoint horizontal
         ymin: bbox minimum vertical (left side on img)
@@ -105,15 +133,15 @@ def get_cylinderheight_from_bbox(x_avg, ymin, ymax, rvec, tvec, K):
         rvec: camera rotation vector (extrinsic parameters)
         tvec: camera translation vector (extrinsic paramters)
         K: camera matrix (intrinsic paramters)
-    
+
     Returns:
         height of 3D cylinder projected from 2D bbox params
-        
+
     """
     # get 2D image upper bbox center on 3D floor and
     # camera center in 3D coordinates
-    X_head_floor, Ctilde  = project_2D_to_3D(x_avg, ymin, rvec, tvec, K)
-    X_foot, _  = project_2D_to_3D(x_avg, ymax, rvec, tvec, K)
+    X_head_floor, Ctilde  = _project_2D_to_3D(x_avg, ymin, rvec, tvec, K)
+    X_foot, _  = _project_2D_to_3D(x_avg, ymax, rvec, tvec, K)
 
     # Define the objective function with P1, P2, P3 as additional arguments
     objective = lambda z4: _objective_function(
@@ -128,22 +156,26 @@ def get_cylinderheight_from_bbox(x_avg, ymin, ymax, rvec, tvec, K):
     result = minimize(objective, initial_guess)
 
     # Get the optimized value of z4
-    z4_optimized = result.x[0]  
+    z4_optimized = result.x[0]
     return z4_optimized
 
 
-def _objective_function(z4, P1, P2, P3):
-    """
-    Compute distance between 3D point and line between two 3d points.
+def _objective_function(
+    z4: float,
+    P1: np.array,
+    P2: np.array,
+    P3: np.array
+) -> float:
+    """Compute distance between 3D point and line between two 3D points.
 
-    More specifically, the 3D point P4=(x2, y2, z4) is above P2=(x2,y2,z2=0) on the
-    ground plane and the line is between P1=(x1,y1,z1) and and P3=(x3,y3,z3=0),
-    also on the ground plane
-    """
+    More specifically, the 3D point P4=(x2, y2, z4) is above P2=(x2,y2,z2=0) on
+    the ground plane and the line is between P1=(x1,y1,z1) and P3=(x3,y3,z3=0),
+    also on the ground plane.
 
+    """
     z4 = z4[0]
     # Calculate the direction vector of the line
-    direction_vector = np.array([P3[0] - P1[0], P3[1] - P1[1], P3[2] - P1[2]])
+    direction_vector = np.float32([P3[0] - P1[0], P3[1] - P1[1], P3[2] - P1[2]])
 
     # Calculate the magnitude of the direction vector
     magnitude = np.linalg.norm(direction_vector)
@@ -152,7 +184,7 @@ def _objective_function(z4, P1, P2, P3):
     unit_direction_vector = direction_vector / magnitude
 
     # Calculate the point P4
-    P4 = np.array([P2[0], P2[1], z4])
+    P4 = np.float32([P2[0], P2[1], z4])
 
     # Calculate the vector from P1 to P4
     vector_P1_P4 = P4 - P1
--- src/mutlicam_wildtrack_3D_cylinder_to_2D_bbox_projections.py
+++ src/mutlicam_wildtrack_3D_cylinder_to_2D_bbox_projections.py
@@ -1,42 +1,76 @@
+"""Transform 3D world grid cylinder parameters to 2D image plane bbox params.
+
+Notation follows Duy's thesis, Appendix B in
+https://arxiv.org/pdf/2111.11892.pdf.
+
+"""
+# pylint: disable=[E1136]
 
 import math
+from typing import Dict
 
 import numpy as np
 from cv2 import Rodrigues
 from cv2 import projectPoints as project_3D_to_2D
 
 
+def transform_3D_cylinder_to_2D_bbox_params(
+    cylinder,
+    rvec,
+    tvec,
+    camera_matrix,
+    dist_coeffs
+) -> Dict:
+    """Transforms 3D cylinder to 2D bbox given camera calibration.
 
-def transform_3D_cylinder_to_2D_bbox_params(cylinder, rvec, tvec, camera_matrix, distcoeffs):
-    
+    Args:
+        cylinder: cylinder data with x_center, y_center, height and radius.
+        rvec: camera rotation vector (extrinsic parameters).
+        tvec: camera translation vector (extrinsic paramters).
+        camera_matrix: camera matrix (intrinsic paramters).
+        dist_coeffs: distortion coefficients.
+
+    Returns:
+        bbox params.
+
+    """
     R, _ = Rodrigues(rvec)
     Ctilde = (- R.T @ tvec).flatten()
     X_foot = np.float32([cylinder["x_center"], cylinder["y_center"], 0])
 
-    X0_lowerright_corner_temp = shift_2D_point_perpendicular(P1=X_foot[0:2], P2=Ctilde[0:2], dist=cylinder["radius"])
+    X0_lowerright_corner_temp = _shift_2D_point_perpendicular(
+        P1=X_foot[0:2],  # pylint: disable=[E1136]
+        P2=Ctilde[0:2],
+        dist=cylinder["radius"]
+    )
+
     X0_lowerright_corner = np.zeros(3)
     X0_lowerright_corner[0:2] = X0_lowerright_corner_temp
 
     x0_lowerright_corner, _ = project_3D_to_2D(
-        X0_lowerright_corner,  # 3D points
-        rvec.flatten(),  # rotation rvec
-        tvec.flatten(),  # translation tvec
+        X0_lowerright_corner,
+        rvec.flatten(),
+        tvec.flatten(),
         camera_matrix,
-        distcoeffs  # camera matrix
+        dist_coeffs
     )
 
     # MINUS radius
-    X0_lowerright_corner_temp = shift_2D_point_perpendicular(P1=X_foot[0:2], P2=Ctilde[0:2], dist=-cylinder["radius"])
+    X0_lowerright_corner_temp = _shift_2D_point_perpendicular(
+        P1=X_foot[0:2],  # pylint: disable=[E1136]
+        P2=Ctilde[0:2],
+        dist=-cylinder["radius"]
+    )
     X0_upperright_corner = np.zeros(3)
     X0_upperright_corner[0:2] = X0_lowerright_corner_temp
     X0_upperright_corner[2] = cylinder["height"]
 
     x0_upperright_corner, _ = project_3D_to_2D(
-        X0_upperright_corner ,  # 3D points
-        rvec.flatten(),  # rotation rvec
-        tvec.flatten(),  # translation tvec
+        X0_upperright_corner,
+        rvec.flatten(),
+        tvec.flatten(),
         camera_matrix,
-        distcoeffs  # camera matrix
+        dist_coeffs
     )
 
     return {
@@ -47,8 +81,12 @@ def transform_3D_cylinder_to_2D_bbox_params(cylinder, rvec, tvec, camera_matrix,
     }
 
 
-def shift_2D_point_perpendicular(P1: np.array, P2: np.array, dist: float) -> np.array:
-    """Shift 2D point P1 perpendicular to line from P1 to P2 in 2D by some distance.
+def _shift_2D_point_perpendicular(
+    P1: np.array,
+    P2: np.array,
+    dist: float
+) -> np.array:
+    """Shift 2D point P1 perpendicular to line P1 to P2 in 2D by a distance.
     
     Args:
         P1: 2D Array defining the 2D point that is shifted.
@@ -57,24 +95,33 @@ def shift_2D_point_perpendicular(P1: np.array, P2: np.array, dist: float) -> np.
 
     Returns:
         new_P1: 2D array of the shifted P1.
-    
+
     """
     x1, y1 = P1
     x2, y2 = P2
 
     # Calculate the direction vector
-    direction_vector = (x2 - x1, y2 - y1)
+    direction_vector = np.float32([x2 - x1, y2 - y1])
 
     # Calculate the magnitude of the direction vector
     magnitude = math.sqrt(direction_vector[0] ** 2 + direction_vector[1] ** 2)
 
     # Normalize the direction vector
-    unit_direction_vector = (direction_vector[0] / magnitude, direction_vector[1] / magnitude)
+    unit_direction_vector = np.float32([
+        direction_vector[0] / magnitude,
+        direction_vector[1] / magnitude
+    ])
 
     # Calculate the displacement vector
-    displacement_vector = (dist * (-unit_direction_vector[1]), dist * unit_direction_vector[0])
+    displacement_vector = np.float32([
+        dist * (-unit_direction_vector[1]),
+        dist * unit_direction_vector[0]
+    ])
 
     # Calculate the new position of P1
-    new_P1 = np.array([x1 + displacement_vector[0], y1 + displacement_vector[1]])
+    new_P1 = np.float32([
+        x1 + displacement_vector[0],
+        y1 + displacement_vector[1]
+    ])
 
     return new_P1
