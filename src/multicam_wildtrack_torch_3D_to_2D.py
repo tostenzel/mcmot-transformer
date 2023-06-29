a15afebc1d621ba85aab55e738f646a7ca1fe70f
tostenzel <tobias.stenzel@mailbox.org>
2023-06-29 15:45:35 +0200

    Vectorized implementation of eval helper function

--- src/multicam_wildtrack_torch_3D_to_2D.py
+++ src/multicam_wildtrack_torch_3D_to_2D.py
@@ -1,5 +1,7 @@
-"""Reimplements `multicam_wildtrack_3D_cylinder_to_2D_bbox_projections.py`
-completely in PyTorch, with bbox return value in COCO format instead WILDTRACK.
+"""Efficient reimplementation of `multicam_wildtrack_3D_cylinder_to_2D_bbox_projections.py`.
+
+Vectorized, completely in PyTorch with bbox return value already in COCO format
+instead of WILDTRACK format.
 
 Will be used for the evaluation in `engine.py`.
 
@@ -20,132 +22,179 @@ from wildtrack_globals import INTRINSIC_CALIBRATION_FILES, \
 from wildtrack_globals import W, H
 
 
-
-def transform_3D_cylinder_to_2D_bbox_params(
+def transform_3D_cylinder_to_2D_COCO_bbox_params(
     cylinder: torch.Tensor,
     rvec: torch.Tensor,
     tvec: torch.Tensor,
     camera_matrix: torch.Tensor,
 ) -> torch.Tensor:
-    """Transforms a 3D cylinder to 2D bbox given camera calibration.
+    """Transforms N 3D cylinders to 2D bbox in COCO given camera calibration.
+
+    If a cylinder can not be seen in the image, return (-1, -1, -1, -1).
 
     Args:
         cylinder: Cylinder data with x_center, y_center, height, and radius.
-        Shape: (4,)
+        Shape: (N, 4)
         ...
 
     Returns:
         Bbox parameters.
-        Shape: (4,)
+        Shape: (N, 4)
 
     """
-
     R = Rodrigues(rvec)
     Ctilde = (-torch.matmul(R.T, tvec)).flatten()
-    X_foot = torch.tensor([cylinder[0], cylinder[1], 0], dtype=torch.float32)
+    X_foot = torch.stack([cylinder[:, 0], cylinder[:, 1], torch.zeros(cylinder.shape[0])], dim=1)
 
-    X0_lowerright_corner_temp = _shift_2D_point_perpendicular(
-        P1=X_foot[0:2],
-        P2=Ctilde[0:2],
-        dist=cylinder[3]
+    X0_lowerright_corner_temp = _shift_2D_points_perpendicular(
+        P1=X_foot[:, :2],
+        P2=Ctilde[:2],  # cam center always the same
+        dist=cylinder[:, 3]
     )
 
-    X0_lowerright_corner = torch.zeros(3)
-    X0_lowerright_corner[0:2] = X0_lowerright_corner_temp
+    #X0_lowerright_corner = torch.zeros_like(X0_lowerright_corner_temp)
+    #X0_lowerright_corner[:, :2] = X0_lowerright_corner_temp
+    X0_lowerright_corner = torch.stack([
+        X0_lowerright_corner_temp[:, 0],
+        X0_lowerright_corner_temp[:, 1],
+        torch.zeros(cylinder.shape[0])
+        ], dim=1)
 
-    x0_lowerright_corner = projectPoint(
+    x0_lowerright_corner = projectPoints(
         X=X0_lowerright_corner,
         rvec=rvec,
         tvec=tvec,
         camera_matrix=camera_matrix
     )
-    print("torch: x0_lowerright_corner", x0_lowerright_corner)
 
     # MINUS radius
-    X0_lowerright_corner_temp = _shift_2D_point_perpendicular(
-        P1=X_foot[0:2],
-        P2=Ctilde[0:2],
-        dist=-cylinder[3]
+    X0_lowerright_corner_temp = _shift_2D_points_perpendicular(
+        P1=X_foot[:, :2],
+        P2=Ctilde[:2],
+        dist=-cylinder[:, 3]
     )
-    X0_upperright_corner = torch.zeros(3)
-    X0_upperright_corner[0:2] = X0_lowerright_corner_temp
-    X0_upperright_corner[2] = cylinder[2]
 
-    x0_upperright_corner = projectPoint(
+    X0_upperright_corner = torch.stack([
+        X0_lowerright_corner_temp[:, 0],
+        X0_lowerright_corner_temp[:, 1],
+        cylinder[:, 2]
+        ],
+        dim=1)
+
+    x0_upperright_corner = projectPoints(
         X=X0_upperright_corner,
         rvec=rvec,
         tvec=tvec,
         camera_matrix=camera_matrix
     )
 
-    bbox = torch.tensor([
-        torch.round(x0_lowerright_corner.flatten()[0]),
-        torch.round(x0_upperright_corner.flatten()[0]),
-        torch.round(x0_lowerright_corner.flatten()[1]),
-        torch.round(x0_upperright_corner.flatten()[1])
-    ], dtype=torch.float32)
+    # see convert_wildtrack_to_coco_bbox in wildtrack_shared.py
+    # xmin, ymin, width, height_box, origin at upper left corner of image
+    bbox = torch.stack([
+        x0_upperright_corner[:, 0],
+        x0_upperright_corner[:, 1],
+        x0_lowerright_corner[:, 0] - x0_upperright_corner[:, 0],
+        x0_lowerright_corner[:, 1] - x0_upperright_corner[:, 1]
+    ]).T
 
-    # Check whether we can see the whole bbox in this camera
-    if (W < bbox[0] < 0) or (W < bbox[1] < 0) \
-    or (H < bbox[2] < 0) or (H < bbox[3] < 0):
-        return torch.tensor([-1, -1, -1, -1], dtype=torch.float32)
-    else:
-        return bbox
+    #---------------------------------------------------------------------------
+    # Conditions if a cylinder is not visible in the image of a camera
+
+    # xmin, ymin must be positive, height and width strichtly positive
+    condition_1 = (bbox[:, 0] < 0) | (bbox[:, 1] < 0) | (bbox[:, 2] < 1) | (bbox[:, 3] < 1)
+    # xmin or xmin + width > W
+    condition_2 = bbox[:, 0] + bbox[:, 2] > W
+    # ymin or ymin + height > H
+    condition_3 = bbox[:, 1] + bbox[:, 3] > H
+
+    # Check if any condition is violated along the row dimension
+    violated = torch.any(torch.stack([condition_1, condition_2, condition_3], dim=1), dim=1)
+
+    # Set rows to (-1, -1, -1, -1) where conditions are violated
+    bbox = torch.where(violated.unsqueeze(1), torch.tensor([-1, -1, -1, -1], dtype=torch.float32), bbox)
+    #---------------------------------------------------------------------------
+
+    return torch.round(bbox)
+
+
+def convert_wildtrack_to_coco_bbox(xmax, xmin, ymax, ymin):
+    """Converts the bbox format used in WILDTRACK to COCO.
+    
+    Both assume that the origin of an image is the upper left pixel.
+    The x and y coordinates for coco represent the upper left bbox corner.
+    
+    """
+    x = xmin
+    y = ymin
+    width = xmax - xmin
+    height_box = ymax - ymin
+    return x, y, width, height_box
 
 
 
-def _shift_2D_point_perpendicular(
-        P1: torch.Tensor,
-        P2: torch.Tensor,
-        dist: torch.Tensor
+def _shift_2D_points_perpendicular(
+    P1: torch.Tensor,
+    P2: torch.Tensor,   # Cam center always the same
+    dist: torch.Tensor
 ) -> torch.Tensor:
     """
-    Shift a 2D point P1 perpendicular to the line from P1 to P2 in 2D by a given distance.
+    Shift 2D points P1 perpendicular to the line from P1 to P2 in 2D by a given distance.
+
+    Args:
+        P1: Tensor of shape (N, 2) representing the coordinates of N 2D points.
+        P2: Flat tensor of shape (2,) representing the coordinates of the corresponding point.
+        dist: Tensor of shape (N,) representing the distance to shift each point.
+
+    Returns:
+        Tensor of shape (N, 2) representing the new coordinates of the shifted points.
 
     """
+    x1, y1 = P1.unbind(dim=1)
+    x2, y2 = P2[0], P2[1]
 
-    x1, y1 = P1
-    x2, y2 = P2
+    # Calculate the direction vectors
+    direction_vectors = torch.stack([x2 - x1, y2 - y1], dim=1)
 
-    # Calculate the direction vector
-    direction_vector = torch.tensor([x2 - x1, y2 - y1], dtype=torch.float32)
+    # Calculate the magnitudes of the direction vectors
+    magnitudes = torch.norm(direction_vectors, dim=1)
 
-    # Calculate the magnitude of the direction vector
-    magnitude = torch.sqrt(direction_vector[0] ** 2 + direction_vector[1] ** 2)
+    # Normalize the direction vectors
+    unit_direction_vectors = direction_vectors / magnitudes.unsqueeze(1)
 
-    # Normalize the direction vector
-    unit_direction_vector = direction_vector / magnitude
+    # Calculate the displacement vectors
+    displacement_vectors = torch.stack([
+        dist * (-unit_direction_vectors[:, 1]),
+        dist * unit_direction_vectors[:, 0]
+    ], dim=1)
 
-    # Calculate the displacement vector
-    displacement_vector = torch.tensor([
-        dist * (-unit_direction_vector[1]),
-        dist * unit_direction_vector[0]
-    ], dtype=torch.float32)
-
-    # Calculate the new position of P1
-    new_P1 = torch.tensor([
-        x1 + displacement_vector[0],
-        y1 + displacement_vector[1]
-    ], dtype=torch.float32)
+    # Calculate the new positions of P1
+    new_P1 = P1 + displacement_vectors
 
     return new_P1
 
 
-def projectPoint(
+
+def projectPoints(
     X: torch.Tensor,
     rvec: torch.Tensor,
     tvec: torch.Tensor,
     camera_matrix: torch.Tensor
 ) -> torch.Tensor:
-    """Project a 3D point onto the image plane similar to cv2.projectPoints.
+    """Project 3D points onto the image plane similar to cv2.projectPoints.
+
+    Args:
+        X: 3D points to project. Shape: (N, 3), where N is the number of points.
+        rvec: Camera rotation vector (extrinsic parameters). Shape: (3, 1).
+        tvec: Camera translation vector (extrinsic parameters). Shape: (3, 1).
+        camera_matrix: Camera matrix (intrinsic parameters). Shape: (3, 3).
 
     Returns:
-        Projected 2D point on the image plane.
-        Shape: (2,)
+        Projected 2D points on the image plane. Shape: (N, 2), where N is the number of points.
+
     """
-    # Create a homogeneous representation of the 3D point
-    X_homo = torch.ones((4, 1), dtype=torch.float32)
-    X_homo[0:3, 0] = X
+    # Create homogeneous representations of the 3D points
+    X_homo = torch.ones((X.shape[0], 4), dtype=torch.float32)
+    X_homo[:, :3] = X
 
     # Convert the rotation vector to a rotation matrix
     R = Rodrigues(rvec)
@@ -156,11 +205,11 @@ def projectPoint(
     # Compute the projection matrix
     P = camera_matrix @ Rt_inhomo
 
-    # Project the point onto the image plane
-    x_homo = P @ X_homo
-    x = x_homo[:2] / x_homo[2]  # Normalize the homogeneous coordinates
+    # Project the points onto the image plane
+    x_homo = torch.matmul(X_homo, P.t())
+    x = x_homo[:, :2] / x_homo[:, 2].unsqueeze(1)  # Normalize the homogeneous coordinates
 
-    return x.flatten()
+    return x
 
 
 def Rodrigues(rvec: torch.Tensor) -> torch.Tensor:
@@ -215,8 +264,3 @@ def load_spec_intrinsics(view: int) -> Tuple[torch.Tensor, torch.Tensor]:
         SRC_CALIBRATION + '/intrinsic_zero/' + _file, 'distortion_coefficients'
     )
     return torch.from_numpy(camera_matrix), torch.from_numpy(distortion_coefficient)
-
-
-
-
-
