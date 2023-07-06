86e80cac99f1540b23a09bfe1421c99def3f6470
tostenzel <tobias.stenzel@mailbox.org>
2023-07-06 08:46:06 +0200

    Validate and test on 2D instead 3D data

--- src/multicam_wildtrack_generate_3d_coco.py
+++ src/multicam_wildtrack_generate_3d_coco.py
@@ -2,6 +2,8 @@
 `data/Wildtrack_dataset` (.png) in sequence-specific folders with
 sequence-specific annotations files (only first file used by model).
 
+We only store 3D data for the train split, but 2D for test and val!!!
+
 In contrast to the module `wildtrack_generate_3d_coco.py`,
 we store 3D cylinder parameters generated from the information of all views
 in the COCO bbox slot. We only do this for the first view/sequence `c0` and load
@@ -32,6 +34,7 @@ from multicam_wildtrack_load_calibration import load_all_intrinsics
 from multicam_wildtrack_load_calibration import load_all_extrinsics
 from multicam_wildtrack_2D_bbox_to_3D_cylinder_projections import \
     transform_2D_bbox_to_3D_cylinder_params as get_cylinder
+from wildtrack_shared import convert_wildtrack_to_coco_bbox
 from wildtrack_generate_coco import _create_coco_files
 from wildtrack_shared import check_coco_from_wildtrack
 from wildtrack_shared import validate_jpgs
@@ -74,7 +77,10 @@ def generate_3D_coco_from_wildtrack() -> None:
     ]
 
     # flexible annotation id for uneven annotation number per camera and sequence
-    train_ann_id, test_ann_id, val_ann_id = 0, 0, 0
+    train_ann_id = 0
+    test_ann_id = glob.TRAIN_SEQ_LENGTH
+    val_ann_id = glob.TRAIN_SEQ_LENGTH + glob.TRAIN_SEQ_LENGTH
+
     output_train_annotation = "train.json"
     output_test_annotation = "test.json"
     output_val_annotation = "val.json"
@@ -154,69 +160,126 @@ def _create_3D_annotations(
     ann_id = start_annotation_id
     images = []
     annotations = []
+    #---------------------------------------------------------------------------
+    # 3d coordinates only in annoation of c0, separate folders and anns per seq
+    if split == "train":
+        # loop length depends mainly on NUM_3D_HEIGHT_GRID_POINTS from
+        # `multicam_wildtrack_2D_bbox_to_3D_cylinder_projections.py`
+        for ann_file in ann_files:
+            data = json.load(open(glob.SRC_ANNS + "/" + ann_file, "r"))  # was .json
 
-    # loop length depends mainly on NUM_3D_HEIGHT_GRID_POINTS from
-    # `multicam_wildtrack_2D_bbox_to_3D_cylinder_projections.py`
-    for ann_file in ann_files:
-        data = json.load(open(glob.SRC_ANNS + "/" + ann_file, "r"))  # was .json
+            image_name = ann_file.rsplit(".", 1)[0] + ".jpg"
+            image_name = f"c{c}-" + ann_file.rsplit(".", 1)[0] + ".jpg"
+            images.append({
+                "file_name": f"{image_name}",
+                "height": glob.H,
+                "width": glob.W,
+                "id": img_id,# + img_id_offset,
+                "license": 1,
+                "frame_id": img_id,
+                "seq_length": seq_length,
+                "first_frame_image_id": 0# + img_id_offset
+            })
+            # generate object specific data (esp. 3D cylinders) only once.
+            if c == 0:
+                for instance in data:
+                    cylinder_list = []
+                    # always use max number of cameras because there may be some
+                    # empty cylinder_arrs that the code does not handle for now
+                    for cam in range(7):
+                        # only use data for the selected camera and not for all others,
+                        # where the same person is also visible
+                        xmax, ymax, xmin, ymin = instance["views"][cam]["xmax"], instance["views"][cam]["ymax"], instance["views"][cam][
+                        "xmin"], instance["views"][cam]["ymin"]
+                        if not (xmax == -1 or ymax == -1 or xmin == -1 or ymin == 1):
+                            #x = xmin
+                            #y = ymin
+                            #w_box = xmax - xmin
+                            #h_box = ymax - ymin
+                            cylinder = get_cylinder(instance["views"][cam], rvecs[cam], tvecs[cam], camera_matrices[cam])
+                            cylinder_arr = np.fromiter(cylinder.values(), dtype=float)
+                            cylinder_list.append(cylinder_arr)
+                        
+                    cylinder_mean = np.mean(cylinder_list, axis=0)
+                    cylinder_list = []
+                    annotations.append({
+                        "id": ann_id,# + annotation_id_offset,
+                        "bbox": [
+                            # rounding not here but in proction from 3D to 2D.
+                            cylinder_mean[0],
+                            cylinder_mean[1],
+                            cylinder_mean[2],
+                            cylinder_mean[3]
+                            ],
+                        "image_id": img_id,# + img_id_offset
+                        "segmentation": [],
+                        "visibility": 1.0,
+                        # "area": w_box * h_box,
+                        "area": -1,
+                        "category_id": 1,
+                        "iscrowd": 0,
+                        "seq": f"c{c}" + seq_name_appendix,
+                        "track_id": instance["personID"]
+                    })
 
-        image_name = ann_file.rsplit(".", 1)[0] + ".jpg"
-        image_name = f"c{c}-" + ann_file.rsplit(".", 1)[0] + ".jpg"
-        images.append({
-            "file_name": f"{image_name}",
-            "height": glob.H,
-            "width": glob.W,
-            "id": img_id,# + img_id_offset,
-            "license": 1,
-            "frame_id": img_id,
-            "seq_length": seq_length,
-            "first_frame_image_id": 0# + img_id_offset
-        })
-        # generate object specific data (esp. 3D cylinders) only once.
-        if c == 0:
+                    ann_id += 1
+            img_id += 1
+    #---------------------------------------------------------------------------
+    # 2D coordinates in annotation of seq in separate folders
+    # straight copy from `wildtrack_generate_coco.py`` except img_id_offset       
+    else:
+        for ann_file in ann_files:
+            data = json.load(open(glob.SRC_ANNS + "/" + ann_file, "r"))  # was .json
+
+            image_name = ann_file.rsplit(".", 1)[0] + ".jpg"
+            image_name = f"c{c}-" + ann_file.rsplit(".", 1)[0] + ".jpg"
+            images.append({
+                "file_name": f"{image_name}",
+                "height": glob.H,
+                "width": glob.W,
+                "id": img_id,# + img_id_offset,
+                "license": 1,
+                # tracking specific
+                # `frame_id` is the img"s position relative to its sequence,
+                # not the whole dataset (0 - 400),
+                # see https://github.com/timmeinhardt/trackformer/issues/33#issuecomment-1105108004
+                # Starts from 1 in MOT format
+                "frame_id": img_id,
+                "seq_length": seq_length,
+                "first_frame_image_id": 0# + img_id_offset
+            })
+
             for instance in data:
-                cylinder_list = []
-                # always use max number of cameras because there may be some
-                # empty cylinder_arrs that the code does not handle for now
-                for cam in range(7):
-                    # only use data for the selected camera and not for all others,
-                    # where the same person is also visible
-                    xmax, ymax, xmin, ymin = instance["views"][cam]["xmax"], instance["views"][cam]["ymax"], instance["views"][cam][
-                    "xmin"], instance["views"][cam]["ymin"]
-                    if not (xmax == -1 or ymax == -1 or xmin == -1 or ymin == 1):
-                        #x = xmin
-                        #y = ymin
-                        #w_box = xmax - xmin
-                        #h_box = ymax - ymin
-                        cylinder = get_cylinder(instance["views"][cam], rvecs[cam], tvecs[cam], camera_matrices[cam])
-                        cylinder_arr = np.fromiter(cylinder.values(), dtype=float)
-                        cylinder_list.append(cylinder_arr)
-                    
-                cylinder_mean = np.mean(cylinder_list, axis=0)
-                cylinder_list = []
-                annotations.append({
-                    "id": ann_id,# + annotation_id_offset,
-                    "bbox": [
-                        # rounding not here but in proction from 3D to 2D.
-                        cylinder_mean[0],
-                        cylinder_mean[1],
-                        cylinder_mean[2],
-                        cylinder_mean[3]
-                        ],
-                    "image_id": img_id,# + img_id_offset
-                    "segmentation": [],
-                    "visibility": 1.0,
-                    # "area": w_box * h_box,
-                    "area": -1,
-                    "category_id": 1,
-                    "iscrowd": 0,
-                    "seq": f"c{c}" + seq_name_appendix,
-                    "track_id": instance["personID"]
-                })
+                # only use data for the selected camera and not for all others,
+                # where the same person is also visible
+                xmax, ymax, xmin, ymin = instance["views"][c]["xmax"], instance["views"][c]["ymax"], instance["views"][c][
+                "xmin"], instance["views"][c]["ymin"]
+                if not (xmax == -1 or ymax == -1 or xmin == -1 or ymin == 1):
+                    x, y, w_box, h_box = convert_wildtrack_to_coco_bbox(xmax, xmin, ymax, ymin)
+                    annotations.append({
+                        "id": ann_id,# + annotation_id_offset,
+                        "bbox": [
+                            int(round(x)),
+                            int(round(y)),
+                            int(round(w_box)),
+                            int(round(h_box))
+                            ],
+                        "image_id": img_id,# + img_id_offset, #+ val_img_id_offset,
+                        "segmentation": [],
+                        #"ignore":,
+                        "visibility": 1.0,
+                        "area": w_box * h_box,
+                        "category_id": 1,
+                        "iscrowd": 0,
+                        # tracking specific
+                        "seq": f"c{c}" + seq_name_appendix,
+                        # TODO: perhaps offset, too? Yet, this info should make baseline stronger.
+                        "track_id": instance["personID"]
+                    })
 
                 ann_id += 1
-        img_id += 1
-
+            img_id += 1
+    #---------------------------------------------------------------------------
     return images, annotations, ann_id
 
 
@@ -236,21 +299,23 @@ if __name__ == "__main__":
         )
 
         check_coco_from_wildtrack(
-            three_dim_multicam=True,
+            three_dim_multicam=False,
             img_dir_path = f"{glob.MULTICAM_ROOT}/{id_}/val",
             write_path = f"{glob.MULTICAM_ROOT}/debug_coco_images",
             # Fix to c0
-            coco_annotations_path = f"{glob.MULTICAM_ROOT}/c0/annotations/val.json",
-            num_img=5
+            coco_annotations_path = f"{glob.MULTICAM_ROOT}/{id_}/annotations/val.json",
+            num_img=5,
+            no_img_id_offset=True
         )
 
         check_coco_from_wildtrack(
-            three_dim_multicam=True,
+            three_dim_multicam=False,
             img_dir_path = f"{glob.MULTICAM_ROOT}/{id_}/test",
             write_path = f"{glob.MULTICAM_ROOT}/debug_coco_images",
             # Fix to c0
-            coco_annotations_path = f"{glob.MULTICAM_ROOT}/c0/annotations/test.json",
-            num_img=5
+            coco_annotations_path = f"{glob.MULTICAM_ROOT}/{id_}/annotations/test.json",
+            num_img=5,
+            no_img_id_offset=True
         )
 
     validate_jpgs(multicam=True)
